c13157c338f4526a7dbc674b8ff5458e9ed9e915
Lakshmi Prasanna Rangam <LakshmiPrasannaRangam>
2020-07-20 22:08:47 -0600

    added files

---
+++ static/app.js
@@ -0,0 +1,9 @@
+
+//Load the data and Data Cleaning - d3.json (api)
+d3.json('/api/suicides').then(function(data){
+  console.log(data) 
+});
+
+      
+
+   
---
+++ static/js/config.js
@@ -0,0 +1,3 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibGFrc2htaTgzIiwiYSI6ImNrY3A4c3o4eTBkYzMycXM2em14am5uNmcifQ.lX4chHIVr_b-9pweDTz3qA";
+
